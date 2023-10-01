68f609050735ba4146f17f1d982d7a50e729e49d
Ramesh Polishetti <4971903+rpolishetti@users.noreply.github.com>
2023-10-01 07:49:14 -0700

    enable core vitals

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,7 +1,7 @@
 const reportWebVitals = (onPerfEntry) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      console.log(getCLS(onPerfEntry));
+      getCLS(onPerfEntry);
       getFID(onPerfEntry);
       getFCP(onPerfEntry);
       getLCP(onPerfEntry);
